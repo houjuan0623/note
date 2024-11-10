93a6ad661f0c740c5c517866d480402ec6baea1c
seconp <15290631595@163.com>
2024-11-10 20:39:03 +0800

    添加molecular包。从官网下载的0.14.34版本。

---
+++ daily_note/molecular/moleculer-0.14.34/test/typescript/tsd/ServiceSchema_lifecycles.test-d.ts
@@ -0,0 +1,52 @@
+import { expectType } from "tsd";
+import { Service, ServiceBroker, ServiceSchema } from "../../../index";
+
+const broker = new ServiceBroker({ logger: false, transporter: "fake" });
+
+class TestService1 extends Service {
+	constructor(broker: ServiceBroker) {
+		super(broker);
+
+		this.parseServiceSchema({
+			name: "test1",
+			created() {},
+			async started() {},
+			async stopped() {}
+		});
+	}
+}
+
+class TestService2 extends Service {
+	constructor(broker: ServiceBroker) {
+		super(broker);
+
+		this.parseServiceSchema({
+			name: "test2",
+			created() {},
+			started() {},
+			stopped() {}
+		});
+	}
+}
+
+class TestService3 extends Service {
+	constructor(broker: ServiceBroker) {
+		super(broker);
+
+		this.parseServiceSchema({
+			name: "test3",
+			created: [() => {}, () => {}],
+			started: [async () => {}, () => {}],
+			stopped: [async () => {}, () => {}]
+		});
+	}
+}
+
+const testService1 = new TestService1(broker);
+expectType<ServiceSchema>(testService1.schema);
+
+const testService2 = new TestService2(broker);
+expectType<ServiceSchema>(testService2.schema);
+
+const testService3 = new TestService3(broker);
+expectType<ServiceSchema>(testService3.schema);
