ff82a3481381751b0c95d202b5bcc34324d3f34d
seconp <15290631595@163.com>
2024-12-06 13:21:08 +0800

    add react repository

---
+++ react/aboutReact/react-code-source/src/main1.tsx
@@ -0,0 +1,14 @@
+import ReactDOM from "@/react-dom"
+
+const App:any =function(){
+    return(
+        <h1>
+            <h2>
+                <h3>3333</h3>
+            </h2>
+        </h1>
+    )
+}
+const root: any = document.querySelector('#root');
+debugger
+ReactDOM.createRoot(root).render(App);
